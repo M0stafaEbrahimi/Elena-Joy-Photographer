176e8e62e95a98c27b9130c65ab897642a44021f
M0stafaEbrahimi <mse.researcher@gmail.com>
2021-05-19 23:44:05 +0430

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const nav = document.querySelector('.nav-links');
+const burger = document.querySelector('.burger');
+const links = document.querySelectorAll('a');
+
+burger.addEventListener('click',() => {
+    nav.classList.toggle('nav-open')
+    burger.classList.toggle('toggle')
+})
+links.forEach(link => {
+    link.addEventListener('click',()=> {
+        nav.classList.toggle('nav-open')
+        burger.classList.toggle('toggle')
+    })
+})
